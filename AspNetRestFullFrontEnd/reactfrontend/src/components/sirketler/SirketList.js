79eefc66e0e817e837f3db7badd7b9ef599afa93
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-02-11 15:45:18 +0300

    React Front End
    React Front End

---
+++ AspNetRestFullFrontEnd/reactfrontend/src/components/sirketler/SirketList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class SirketList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
