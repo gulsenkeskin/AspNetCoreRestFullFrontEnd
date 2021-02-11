0419a52d0e6a0b6b8260df7f34919cd4e065b952
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-02-11 17:57:32 +0300

    .

--- AspNetRestFullFrontEnd/reactfrontend/src/components/sirketler/SirketList.js
+++ AspNetRestFullFrontEnd/reactfrontend/src/components/sirketler/SirketList.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class SirketList extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Şirketler</h3>
-            </div>
-        )
-    }
+class SirketList extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Şirketler</h3>
+      </div>
+    );
+  }
 }
 
+export default connect()(SirketList);
--- AspNetRestFullFrontEnd/reactfrontend/src/redux/reducers/configureStore.js
+++ AspNetRestFullFrontEnd/reactfrontend/src/redux/reducers/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore } from "redux";
 import rootReducer from "./index";
+import thunk from 'redux-thunk';
 
 export default function configureStore() {
   return createStore(rootReducer);
